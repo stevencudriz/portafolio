322ab4f8839f3cbf7f1969fb7e39447ce87739cc
stevencudriz <51980629+stevencudriz@users.noreply.github.com>
2022-04-27 16:27:58 -0500

    Update header.js

--- src/header.js
+++ src/header.js
@@ -1,3 +1,8 @@
+import React from 'react';
+
+function Header(){
+  return(
+    <>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Navbar</a>
@@ -25,3 +30,6 @@
     </div>
   </div>
 </nav>
+    </>
+    )
+}
