fb50e260669cbc64f2bc36097820199b5201e536
stevencudriz <51980629+stevencudriz@users.noreply.github.com>
2022-04-27 16:48:38 -0500

    Update header.js

--- src/header.js
+++ src/header.js
@@ -5,26 +5,26 @@ function Header(){
    
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" >Navbar</a>
+    <a class="navbar-brand" href="#inicio">Navbar</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link" >Inicio</a>
+          <a class="nav-link" href="#inicio">Inicio</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" >Sobre mí</a>
+          <a class="nav-link" href="#sobre-mi">Sobre mí</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" >Servicios</a>
+          <a class="nav-link" href="#servicios">Servicios</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link">Habilidades</a>
+          <a class="nav-link" href="#habilidades">Habilidades</a>
         </li>
         <li class="nav-item">
-          <a class="btn btn-primary">Cotizar proyecto</a>
+          <a class="btn btn-primary" href="#cotizar-proyecto">Cotizar proyecto</a>
         </li>
       </ul>
     </div>
