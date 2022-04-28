d647f5e5ec255a04743b47b59348eda7ad2fe1ed
stevencudriz <51980629+stevencudriz@users.noreply.github.com>
2022-04-28 12:14:34 -0500

    Update seccion2.js

--- src/seccion2.js
+++ src/seccion2.js
@@ -8,11 +8,11 @@ function Seccion2(){
     
     <div class="container-fluid w-100">
       <div class="col-md-6">
-        <h2>{titulo}</h2>
-        <p>Desarrollador web con 2 años de experiencia, con enfoque en CMS y desarrollo a la medida.</p>
+        <h2 class="titulo2">{titulo}</h2>
+        <p class="texto2">Desarrollador web con 2 años de experiencia, con enfoque en CMS y desarrollo a la medida.</p>
 
-        <h3>¿Porqué elegirme?</h3>
-        <p>Trabajo en conjunto para organizar tus ideas y llevar tu proyecto a la realidad. </p>
+        <h3 class="titulo2-sm">¿Porqué elegirme?</h3>
+        <p class="texto2">Trabajo en conjunto para organizar tus ideas y llevar tu proyecto a la realidad. </p>
       </div>
       <div class="col-md-6 fondo2">
       </div>
