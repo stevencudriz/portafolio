9e7f57b4f2b4414753ed79fbbee768d58b8b785d
stevencudriz <51980629+stevencudriz@users.noreply.github.com>
2022-04-28 00:03:59 -0500

    Update seccion1.js

--- src/seccion1.js
+++ src/seccion1.js
@@ -4,13 +4,13 @@ import pc from './lambda/pc.gif';
 function Seccion1 (){
   
   return(
-    <div class="container-fluid w-100 fondo1">
+    <div class="container-fluid w-100 fondo1" align="center">
       <div class="row">
         <div class="col-md-6 text-left">
           <h1 class="subtitulo1">Steven Cudriz, web dev</h1>
           <h1 class="titulo1">Bievenido a mi portafolio</h1>
         </div>
-        <div class="col-md-6 poligono">
+        <div class="col-md-6 poligono text-center">
           <img src={pc} alt="hola" class="pc"/>
         </div>
       </div>
