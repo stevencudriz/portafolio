f630a83f2d263468b03baed5231708f4cd3647c5
stevencudriz <51980629+stevencudriz@users.noreply.github.com>
2022-04-27 22:37:03 -0500

    Update seccion1.js

--- src/seccion1.js
+++ src/seccion1.js
@@ -6,7 +6,15 @@ function Seccion1 (){
   
   return(
     <div class="container w-100 fondo1">
-    
+      <div class="row">
+        <div class="col-md-6 text-left">
+          <h1 class="subtitulo1">Steven Cudriz, web dev</h1>
+          <h1 class="titulo1">Bievenido a mi portafolio</h1>
+        </div>
+        <div class="col-md-6 poligono">
+          <img src={pc}/>
+        </div>
+      </div>
     </div>
   );
   
