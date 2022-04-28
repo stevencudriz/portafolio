6bba5dff6a556291e0f8e027543831cdba3c17f9
stevencudriz <51980629+stevencudriz@users.noreply.github.com>
2022-04-27 22:29:34 -0500

    Update seccion1.js

--- src/seccion1.js
+++ src/seccion1.js
@@ -1,1 +1,16 @@
+import React from 'react';
+import poligono from './lambda/Polygon 1.png';
+import pc from './lambda/pc.png';
+
+function Seccion1 (){
+  
+  return(
+    <div class="container w-100 fondo1">
+    
+    </div>
+  );
+  
+}
+
+export default Seccion1;
 
