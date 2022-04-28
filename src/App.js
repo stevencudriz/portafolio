cd3a96cc698ff5009d21f331dcb9228ab5959da0
stevencudriz <51980629+stevencudriz@users.noreply.github.com>
2022-04-27 22:45:49 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from "react"
 import "./App.css"
 import Header from "./header";
 import Seccion1 from './seccion1'; 
-import Seccion2 from './seccion2'; 
-import Seccion3 from './seccion3'; 
-import Seccion4 from './seccion4'; 
-import Seccion5 from './seccion5'; 
-import Seccion6 from './seccion6'; 
+//import Seccion2 from './seccion2'; 
+//import Seccion3 from './seccion3'; 
+//import Seccion4 from './seccion4'; 
+//import Seccion5 from './seccion5'; 
+//import Seccion6 from './seccion6'; 
 
 function App (){
 
