6e4a1bde43c920a63063dc7ac736a19b3f9fbf47
stevencudriz <51980629+stevencudriz@users.noreply.github.com>
2022-04-30 13:42:11 -0500

    Update seccion3.js

--- src/seccion3.js
+++ src/seccion3.js
@@ -10,11 +10,10 @@ function Seccion3(){
       <div class="row justify-content-center">
         <div class="col-md-12"><h1 class="titulo3">Mis servicios</h1></div>
         <div class="col-md-4 servicio1">
-         <div class="card">
-          <div class="card-header text-center">
-           <img src={servicio1} alt="" />
-          </div>
-          <div class="card-body">
+         
+           <img src={servicio1} alt="" class="text-center"/>
+         
+   
             <p>Desarrollo de paginas web</p>
             <ul>
               <li>Landing pages</li>
@@ -22,36 +21,24 @@ function Seccion3(){
               <li>Blogs</li>
               <li>Portales web</li>
             </ul>
-          </div>
-         </div>
+
         </div>
 
        <div class="col-md-4 servicio2">
-         <div class="card">
-          <div class="card-header text-center">
            <img src={servicio2} alt="" />
-          </div>
-          <div class="card-body">
             <p>App web a la medida</p>
             <ul>
               <li>Desarrollo tu app adaptada a tus requerimientos</li>
             </ul>
-          </div>
-         </div>
+          
         </div>
 
         <div class="col-md-4 servicio3">
-          <div class="card">
-          <div class="card-header text-center">
            <img src={servicio3} alt="" />
-          </div>
-          <div class="card-body">
             <p>Soporte y arreglo de bugs</p>
             <ul>
               <li>Realizo ajustes a tu web y la optimizo al 100</li>
             </ul>
-          </div>
-         </div>
         </div>
 
       </div>
