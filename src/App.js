a3f45f1cf69b077cf7c899d38187f37329dd7e54
stevencudriz <51980629+stevencudriz@users.noreply.github.com>
2022-04-30 13:20:53 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css"
 import Header from "./header";
 import Seccion1 from './seccion1'; 
 import Seccion2 from './seccion2'; 
-//import Seccion3 from './seccion3'; 
+import Seccion3 from './seccion3'; 
 //import Seccion4 from './seccion4'; 
 //import Seccion5 from './seccion5'; 
 //import Seccion6 from './seccion6'; 
@@ -15,6 +15,7 @@ function App (){
       <Header></Header>
       <Seccion1></Seccion1>
       <Seccion2></Seccion2>
+      <Seccion3></Seccion3>
         </>
     )
 
