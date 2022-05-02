4c98ee29dee741c605dc28788df76d56bc4112d7
stevencudriz <51980629+stevencudriz@users.noreply.github.com>
2022-05-02 16:08:45 -0500

    Update seccion5.js

--- src/seccion5.js
+++ src/seccion5.js
@@ -1,1 +1,13 @@
+import React from 'react';
+import ImageSlider from './ImageSlider';
+import { SliderData } from './SliderData';
 
+function Seccion5(){
+  return(
+    <>
+      <ImageSlider slides={SliderData} />
+    </>
+  );
+}
+
+export default Seccion5;
