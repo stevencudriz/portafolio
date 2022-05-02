2429604b2a4bb7b30df0c0fdf89ca8ffe21147ef
stevencudriz <51980629+stevencudriz@users.noreply.github.com>
2022-05-02 16:08:22 -0500

    Update seccion4.js

--- src/seccion4.js
+++ src/seccion4.js
@@ -1,11 +1,1 @@
-import React from 'react';
-import ImageSlider from './ImageSlider';
-import { SliderData } from './SliderData';
-
-function Seccion4(){
-  return(
-    
-  );
-}
-
-export default Seccion4;
+ 
