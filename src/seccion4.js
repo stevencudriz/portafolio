5e73ed4be54340ecb69b2a8cf48399035c988103
stevencudriz <51980629+stevencudriz@users.noreply.github.com>
2022-05-02 16:28:22 -0500

    Update seccion4.js

--- src/seccion4.js
+++ src/seccion4.js
@@ -1,1 +1,47 @@
- 
+ import React from 'react';
+ import {php} from './lambda/php.png';
+ import {js} from './lambda/js.png';
+ import {css} from './lambda/css.png';
+ import {mysql} from './lambda/mysql.png';
+ import {html} from './lambda/html.png';
+ import {wordpress} from './lambda/wordpress.png';
+
+ function Seccion4(){
+  return(
+   
+   <div class="container-fluid w-100 fondo4">
+    <div class="row justiy-content-center">
+     
+    <div class="col-md-12 text-center">
+     <h1 class="titulo5">Habilidades</h1>
+    </div>
+   
+     <div class="col-md-4 skill">
+      <img src={php} alt="" class="w-70">
+     </div>
+   
+     <div class="col-md-4 skill">
+      <img src={js} alt="" class="w-70">
+     </div>
+   
+     <div class="col-md-4 skill">
+      <img src={css} alt="" class="w-70">
+     </div>
+   
+     <div class="col-md-4 skill">
+      <img src={mysql} alt="" class="w-70">
+     </div>
+   
+     <div class="col-md-4 skill">
+      <img src={html} alt="" class="w-70">
+     </div>
+   
+     <div class="col-md-4 skill">
+      <img src={wordpress} alt="" class="w-70">
+     </div>
+     
+    </div>
+   </div>
+  
+  );
+ }
