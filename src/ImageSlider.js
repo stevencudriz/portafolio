7da8bfab660c6adde04594e76e2cd3fe9db0acdb
stevencudriz <51980629+stevencudriz@users.noreply.github.com>
2022-05-02 16:04:11 -0500

    Update ImageSlider.js

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -26,10 +26,12 @@ const ImageSlider = ({ slides }) => {
         return (
           <div
             className={index === current ? 'slide active' : 'slide'}
-            key={index}
+            
           >
            
+             {index === current && (
               <img src={slide.image}  alt="" className='image' />
+            )}
            
           </div>
         );
